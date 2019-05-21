327fbe2f0cb40bdb6cacf52ed71487b8fd19c8aa
Gaetan Semet <gaetan@xeberon.net>
2019-05-21 23:43:52 +0200

    Update launcher txt

--- install-attractsmc.sh
+++ install-attractsmc.sh
@@ -4,4 +4,4 @@ cp -v attractsmc.sh $HOME/RetroPie/scripts/
 chmod +x $HOME/RetroPie/scripts/attractsmc.sh
 
 sudo rm -f /home/osmc/.kodi/addons/plugin.program.attracsmc-launcher
-sudo cp -fv plugin.program.attracsmc-launcher /home/osmc/.kodi/addons/
+sudo cp -fvr plugin.program.attracsmc-launcher /home/osmc/.kodi/addons/
