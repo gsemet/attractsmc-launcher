41de630d4c1ea3d367bf791839e94d5f189b3c80
Gaetan Semet <gaetan@xeberon.net>
2019-05-21 23:38:44 +0200

    custom install script

--- Makefile
+++
@@ -1,2 +0,0 @@
-plugin:
-	tar cfvz plugin.program.attracsmc-launcher.tar.gz plugin.program.attracsmc-launcher/
--- install-attractsmc.sh
+++ install-attractsmc.sh
@@ -2,3 +2,6 @@
 
 cp -v attractsmc.sh $HOME/RetroPie/scripts/
 chmod +x $HOME/RetroPie/scripts/attractsmc.sh
+
+sudo rm -f /home/osmc/.kodi/addons/plugin.program.attracsmc-launcher
+sudo cp -fv plugin.program.attracsmc-launcher /home/osmc/.kodi/addons/
