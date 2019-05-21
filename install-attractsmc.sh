6af153f0e49c8a4c0b335d57628d75314da1b1f0
Gaetan Semet <gaetan@xeberon.net>
2019-05-21 23:53:05 +0200

    cp -f

--- install-attractsmc.sh
+++ install-attractsmc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp -v attractsmc.sh $HOME/RetroPie/scripts/
+cp -fv attractsmc.sh $HOME/RetroPie/scripts/
 chmod +x $HOME/RetroPie/scripts/attractsmc.sh
 
 sudo rm -f /home/osmc/.kodi/addons/plugin.program.attracsmc-launcher
